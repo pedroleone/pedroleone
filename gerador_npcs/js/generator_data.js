ecc1f4ef5c084be4a3c8c6270377e8f6609a819a
Pedro Luiz Gonçalves <pedro-luiz.goncalves@hpe.com>
2016-10-07 11:40:06 -0300

    Nomes élficos

--- gerador_npcs/js/generator_data.js
+++ gerador_npcs/js/generator_data.js
@@ -44,7 +44,7 @@ var GenderedWord = {
 
 
 var Racas = {
-    common: ["humano","elfo","anão","halfling"],
+    common: ["humano", "humano", "humano", "humano", "humano","elfo","anão","halfling"],
     uncommon: ["draconato","gnomo","meio-elfo","meio-orc","tiefling"],
     rare: ["goliath","gnomo das profundezas","aarakocra","genasi","drow"]
 }
@@ -77,10 +77,13 @@ var Nomes = {
     "halfling": {"M": ["Alton","Ander","Cade","Corrin","Eldon","Errich","Finnan","Garret","Lindal","Lyle","Merric","Milo","Osborn","Perrin","Reed","Roscoe","Wellby"],
                "F": ["Andry","Bree","Callie","Cora","Euphemia","Jillian","Kithri","Lavinia","Lidda","Merla","Nedda","Paela","Portia","Seraphina","Shaena","Trym","Vani","Verna"],
                "sobrenome": ["Brushgather","Goodbarrel","Greenbottle","High-hill","Hilltopple","Leagalow","Tealeaf","Thorngage","Tosscobble","Underbough"]},
-
+    "elfo": {"M": ["Adran","Aelar","Aramil","Arannis","Aust","Beiro","Berrian","Carric","Enialis","Erdan","Erevan","Galinndan","Hadarai","Heian","Himo","Immeral","Ivellios","Laucian","Mindartis","Paelias","Peren","Quarion","Riardon","Rolen","Soveliss","Thamior","Theren","Varis"],
+               "F": ["Adrie","Althaea","Anastrianna","Andraste","Antinua","Bethrynna","Birel","Caelynn","Drusilia","Enna","Felosial","Ielenia","Jelenneth","Keyleth","Leshanna","Lia","Meriele","Mialee","Naivara","Quelenna","Quillathe","Sariel","Shanairra","Shava","Silaqui","Theirastra","Thia","Vadania","Valanthe","Xanaphia"],
+               "sobrenome": ["Amakiir (Gemflower)", "Amastacia (Starflower)", "Galanodel (Moonwhisper)", "Holimion (Oiamonddew)", "IIphelkiir (Gemblossom)", "Liadon (Silverfrond)", "Meliamne (Oakenheel)", "Nailo (Nightbreeze)", "Siannodel (Moonbrook)", "Xiloscient (Goldpetal)"]},
+/*
     "_": {"M": [""],
                "F": [""],
                "sobrenome": [""]},
-
+*/
 }
 
