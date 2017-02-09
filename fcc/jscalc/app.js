6d5ece24a3e3fb2603a93341f76280d7b595147f
Pedro Leone <same@example.com>
2017-02-08 23:12:30 -0200

    Initial work on the JS calculator

--- fcc/jscalc/app.js
+++ fcc/jscalc/app.js
@@ -1,1 +1,40 @@
 'use strict';
+$(function() {
+    var display = [];
+    $(".calc").click(function (){
+        var id = "";
+        //var id = $(this).attr("id");
+        //console.log(id);
+        switch($(this).attr("id")) {
+            case 'plus':
+                id = "+";
+                break;
+            case 'point':
+                id = ".";
+                break;
+            case 'minus':
+                id = "-";
+                break;
+            case 'division':
+                id = '/';
+                break;
+            case 'mult':
+                id = '*';
+                break;
+            case 'eq':
+                id = '=';
+                break;
+            default:
+                id = $(this).attr("id");
+
+        }
+        display.push(id);
+        displayNumbers();
+    }); 
+
+    function displayNumbers(){
+        $(".calc-result").text(display.join(""));
+    }
+
+
+});
